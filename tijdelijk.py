fa144ddf23a012a8a2a060ff0a73276938909d34
Gokhan Yagci <quantumcrypto61@gmail.com>
2025-01-24 22:09:26 +0100

    Eerste versie van tijdelijk.py

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,23 +1,5 @@
 prijzen = {
-    "aardbei": 3,
-    "vanille": 4,
-    "chocolade": 5,
+    "aardbei"
+    "vanille"
+    "chocolade"
 }
-
-aanbieding = prijzen["aardbei"] * 0.8
-
-reclame_tekst = f"vandaag is de aanbieding: vanille-ijs 1 liter - slechts € {aanbieding}"
-
-reclame_tekst2 = reclame_tekst[:reclame_tekst.index("0") + 1]
-
-reclame_tekst3 = reclame_tekst2.upper()
-
-reclame_tekst4 = reclame_tekst3.split()
-
-for el in reclame_tekst4:
-    if len(el) >=5:
-        print(el.upper())
-
-    else:
-        print(el.lower())
-        
